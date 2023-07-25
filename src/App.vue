dee726845c90f0cfa9e4c50f4c0a41b4aed42b0a
ostenbaken87 <ostenbaken87@mail.ru>
2023-07-25 17:32:03 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,108 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <form @submit.prevent class="form">
+      <h4>Создание поста</h4>
+      <input
+          v-bind:value="title"
+          @input="title = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="Название поста"
+      >
+      <input
+          v-bind:value="body"
+          @input="body = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="Описание поста"
+      >
+      <button class="btn"
+              @click="createPost">
+        Создать
+      </button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название:</strong>{{ post.title }}</div>
+      <div><strong>Описание:</strong>{{ post.body }}</div>
+    </div>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'JavaScript', body: 'Описание поста'},
+        {id: 2, title: 'JavaScript2', body: 'Описание поста2'},
+        {id: 3, title: 'JavaScript3', body: 'Описание поста3'},
+        {id: 4, title: 'JavaScript4', body: 'Описание поста4'},
+      ],
+      title: '',
+      body: '',
+    }
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      }
+      this.posts.push(newPost);
+      this.title = '';
+      this.body = '';
+    },
+  }
+}
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+.app {
+  padding: 20px;
 }
 
-nav a {
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+  border-radius: 5px;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 10px;
+  border-radius: 5px;
+}
+
+.btn {
+  align-self: flex-end;
+  margin-top: 10px;
+  padding: 10px 10px;
+  color: teal;
   font-weight: bold;
-  color: #2c3e50;
+  background: none;
+  border: 1px solid teal;
+  border-radius: 5px;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.btn:hover {
+  color: aliceblue;
+  background-color: teal;
+  cursor: pointer;
 }
 </style>

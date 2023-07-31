5947ac72ee4eacb37900ed094f0d1db480182fb4
ostenbaken87 <ostenbaken87@mail.ru>
2023-07-31 17:09:43 +0800

    Routing and Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,165 +1,17 @@
 <template>
+  <navbar></navbar>
   <div class="app">
-    <h1>Страница с постами</h1>
-    <my-input
-        v-model="searchQuery"
-        placeholder="Search..."
-    >
-    </my-input>
-    <div class="app__btns">
-      <my-button
-          @click="showDialog">
-        Создать пост
-      </my-button>
-
-      <my-select
-          v-model="selectedSort"
-          :options="sortOptions"
-      >
-      </my-select>
-    </div>
-
-    <my-dialog v-model:show="dialogVisible">
-      <post-form
-          @create="createPost"
-      />
-    </my-dialog>
-
-    <post-list
-        :posts="sortedAndSearchedPosts"
-        @remove="removePost"
-        v-if="!isPostsLoading"
-    />
-    <div v-else>Loading...</div>
-    <div ref="observer" class="observer"></div>
-    <!--    <div class="page__wrapper">-->
-    <!--      <div-->
-    <!--          v-for="pageNumber in totalPages"-->
-    <!--          :key="pageNumber"-->
-    <!--          class="page"-->
-    <!--          :class="{'current-page': page === pageNumber}"-->
-    <!--          @click="changePage(pageNumber)"-->
-    <!--      >-->
-    <!--        {{pageNumber}}-->
-    <!--      </div>-->
-    <!--    </div>-->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import PostList from "@/components/PostList.vue";
-import PostForm from "@/components/PostForm.vue";
-import MyDialog from "@/components/UI/MyDialog.vue";
-import MyButton from "@/components/UI/MyButton.vue";
-import axios from "axios";
-import MySelect from "@/components/UI/MySelect.vue";
-import MyInput from "@/components/UI/MyInput.vue";
-import {entries} from "core-js/internals/object-to-array";
+import Navbar from "@/components/UI/Navbar.vue";
 
 export default {
-  components: {
-    MyInput,
-    MySelect,
-    MyButton,
-    MyDialog,
-    PostList,
-    PostForm,
-  },
-  data() {
-    return {
-      posts: [],
-      dialogVisible: false,
-      isPostsLoading: false,
-      selectedSort: '',
-      searchQuery: '',
-      page: 1,
-      limit: 10,
-      totalPages: 0,
-      sortOptions: [
-        {value: 'title', name: 'for title'},
-        {value: 'body', name: 'for body'},
-      ]
-    }
-  },
-  methods: {
-    createPost(post) {
-      this.posts.push(post);
-      this.dialogVisible = false;
-    },
-    removePost(post) {
-      this.posts = this.posts.filter(p => p.id !== post.id)
-    },
-    showDialog() {
-      this.dialogVisible = true;
-    },
-    // changePage(pageNumber) {
-    //   this.page = pageNumber
-    // },
-    async fetchPosts() {
-      try {
-        this.isPostsLoading = true;
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
-          params: {
-            _page: this.page,
-            _limit: this.limit,
-          }
-        });
-        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
-        this.posts = response.data;
-        this.isPostsLoading = false;
-      } catch (e) {
-        alert('Error')
-      } finally {
-        this.isPostsLoading = false;
-      }
-    },
-    async loadMorePosts() {
-      try {
-        this.page += 1;
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
-          params: {
-            _page: this.page,
-            _limit: this.limit,
-          }
-        });
-        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
-        this.posts = [...this.posts, ...response.data];
-        this.isPostsLoading = false;
-      } catch (e) {
-        alert('Error')
-      }
-    }
-  },
-  mounted() {
-    this.fetchPosts();
-    console.log(this.$refs.observer);
-    const options = {
-      rootMargin: '0px',
-      threshold: 1.0
-    }
-    const callback = (entries, observer) => {
-      if (entries[0].isIntersecting && this.page < this.totalPages) {
-        this.loadMorePosts()
-      }
-    }
-    const observer = new IntersectionObserver(callback, options);
-    observer.observe(this.$refs.observer);
-  },
-  computed: {
-    sortedPosts() {
-      return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
-    },
-    sortedAndSearchedPosts() {
-      return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
-    },
-  },
-  watch: {
-    // page() {
-    //   this.fetchPosts()
-    // },
-  },
-}
+  components: {Navbar}
 
+}
 </script>
 
 <style>
@@ -171,34 +23,5 @@ export default {
 
 .app {
   padding: 20px;
-}
-
-.app__btns {
-  display: flex;
-  justify-content: space-between;
-  margin: 15px 0;
-}
-
-.page__wrapper {
-  display: flex;
-  margin-top: 15px;
-}
-
-.page {
-  border: 1px solid teal;
-  padding: 10px;
-  color: teal;
-  cursor: pointer;
-}
-
-.current-page {
-  border: 2px solid teal;
-  font-weight: bold;
-  color: black;
-}
-
-.observer {
-  height: 30px;
-  background: teal;
 }
 </style>
---
+++ src/components/UI/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="navbar">
+    <div @click="$router.push('/')" class="navbar__title">Vue 3</div>
+    <div class="navbar__btns">
+      <my-button @click="$router.push('/posts')" class="navbar__btn">Posts</my-button>
+      <my-button @click="$router.push('/about')" class="navbar__btn">About</my-button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+
+
+}
+</script>
+
+<style scoped>
+.navbar {
+  height: 50px;
+  background: lightgrey;
+  box-shadow: 2px 2px 4px gray;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+}
+
+.navbar__btns {
+  margin-left: auto;
+}
+
+.navbar__btn {
+  margin-right: 10px;
+}
+
+.navbar__title {
+  font-weight: bold;
+  font-size: 30px;
+  color: teal;
+  cursor: pointer;
+}
+</style>
